e8c4fcb959437f029c3c516066be4574e2ee9160
Paul <>
2025-05-14 03:13:34 +0200

    Nice highlighting

--- lib/Blame.ex
+++ lib/Blame.ex
@@ -3,35 +3,16 @@ defmodule Blame do
     traverse(ast, {0, 0, "S"}, {cause_ids, fn_name, arity})
   end
 
-  defp highlight(print_stmt, responsible, neutral \\ false) do
-    if responsible do
-      if neutral do
-        IO.ANSI.blue() <> print_stmt <> IO.ANSI.reset()
-      else
-        IO.ANSI.red() <> print_stmt <> IO.ANSI.reset()
-      end
-    else
-      print_stmt
-    end
-  end
-
   defp highlight(print_stmt, responsible, color \\ :red) do
     if responsible do
       prefix =
         case color do
-          :red ->
-            IO.ANSI.red()
-
-          :blue ->
-            IO.ANSI.blue()
-
-          :gray ->
-            IO.ANSI.gray()
+          :red -> IO.ANSI.red()
+          :blue -> IO.ANSI.blue()
+          :gray -> IO.ANSI.color(1, 1, 1)
         end
 
-      prefix <>
-        print_stmt <>
-        IO.ANSI.reset()
+      prefix <> print_stmt <> IO.ANSI.reset()
     else
       print_stmt
     end
@@ -54,7 +35,7 @@ defmodule Blame do
             highlight(
               "if #{Macro.to_string(condition)} do #" <> true_id,
               cause_true or cause_false,
-              cause_false
+              if(cause_false, do: :blue, else: :red)
             )
         )
 
@@ -62,14 +43,18 @@ defmodule Blame do
 
         IO.puts(
           String.duplicate(" ", depth * 2) <>
-            highlight("else #" <> false_id, cause_false)
+            highlight(
+              "else #" <> false_id,
+              cause_true or cause_false,
+              if(cause_true, do: :gray, else: :red)
+            )
         )
 
         traverse(else_clause, {depth + 1, 0, false_id}, config)
 
         IO.puts(
           String.duplicate(" ", depth * 2) <>
-            highlight("end", cause_true or cause_false, cause_false)
+            highlight("end", cause_true or cause_false, if(cause_false, do: :blue, else: :red))
         )
 
       [do: do_clause] ->
@@ -85,23 +70,17 @@ defmodule Blame do
   end
 
   defp traverse(
-         {:defmodule, _meta, [{:__aliases__, __meta, [name]}, [mod_content]]},
-         {depth, _ctr, _acc},
+         {:defmodule, _meta, [{:__aliases__, __meta, _}, [mod_content]]},
+         traverse_data,
          config
        ) do
-    # IO.puts("defmodule #{inspect(name)} do")
-
     case mod_content do
       {:do, stmts} ->
-        traverse(stmts, {depth, 0, "S"}, config)
+        traverse(stmts, traverse_data, config)
 
       _ ->
         nil
-        # IO.inspect("===")
-        # IO.inspect(mod_content)
     end
-
-    # IO.puts("end")
   end
 
   defp traverse(
@@ -123,13 +102,8 @@ defmodule Blame do
     )
   end
 
-  defp traverse(
-         stmt = {decl_type, _meta, _},
-         {depth, _ctr, _acc},
-         _config
-       )
-       when decl_type in [:def, :defp] do
-    walk(stmt, depth)
+  defp traverse({decl_type, _, _}, _traverse_data, _config) when decl_type in [:def, :defp] do
+    nil
   end
 
   defp traverse({:__block__, _, list}, {depth, _ctr, acc}, config) do
@@ -140,7 +114,7 @@ defmodule Blame do
     end)
   end
 
-  defp traverse(other, {depth, _, acc}, {cause_ids, _, _}) do
+  defp traverse(other, {depth, _, _}, _) do
     case other do
       {:@, _, _} ->
         nil
@@ -151,15 +125,5 @@ defmodule Blame do
             "#{Macro.to_string(stmt)}"
         )
     end
-  end
-
-  defp walk(x, depth) do
-    # padding = String.duplicate(" ", depth * 2)
-
-    # Macro.to_string(x)
-    # |> String.split("\n")
-    # |> Enum.map(&"#{padding}#{&1}")
-    # |> Enum.join("\n")
-    # |> IO.puts()
   end
 end
